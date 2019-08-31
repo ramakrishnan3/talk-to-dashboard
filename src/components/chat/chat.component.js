50c7b03599c2221ac35e425102efe482383c16ee
ramakrishnan3 <ramrocks4eva@gmail.com>
2019-08-31 05:25:02 -0700

    adding dashboard and index

---
+++ src/components/chat/chat.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './chat.component.css';
+
+class Chat extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+        <div class="chat-container">
+            
+        </div>
+    )
+  }
+}
+export default Chat;
---
+++ src/components/dashboard/dashboard.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './dashboard.component.css';
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+        <div class="dashboard-container">
+            
+        </div>
+    )
+  }
+}
+export default Dashboard;
