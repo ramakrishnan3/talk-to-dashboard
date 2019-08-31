fb32c7ff8300891d7e180372aff9129e7fd1c29e
ramakrishnan3 <ramrocks4eva@gmail.com>
2019-08-31 05:39:10 -0700

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Chat from './components/chat/chat.component';
 import Dashboard from './components/dashboard/dashboard.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Chat />
+      <div class="header">
+        <Header />
+      </div>
+      <div class="workspace">
+        <Dashboard />
+        <Chat />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/header/header.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.component.css';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+        <div class="header-container">
+            
+        </div>
+    )
+  }
+}
+export default Header;
